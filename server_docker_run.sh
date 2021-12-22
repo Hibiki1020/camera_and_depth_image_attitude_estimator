fdfc3f23044fcb08d1ba7ef2446c0f2b2b397734
Hibiki1020 <hibikijitaku@gmail.com>
2021-12-22 16:09:36 +0900

    Dockerfile

--- server_docker_run.sh
+++ server_docker_run.sh
@@ -12,5 +12,5 @@ docker run -it \
 	--env="QT_X11_NO_MITSHM=1" \
     --name="mono_and_depth_image_attitude_estimator" \
     --volume="$script_dir/:/home/pycode/$image_name/" \
-    --volume="/media/amsl/96fde31e-3b9b-4160-8d8a-a4b913579ca21/:/home/ssd_dir/" \
+    --volume="/home/kawai/ssd_dir/:/home/ssd_dir/" \
     $image_name:$tag_name
